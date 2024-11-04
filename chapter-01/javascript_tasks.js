8e914659aa42cbca0a75484ee3af5daef01fa386
Grekz <Grekz@users.noreply.github.com>
2024-11-04 19:36:23 +0100

    Add javascript beginner tasks (TODOs)

---
+++ chapter-01/javascript_tasks.js
@@ -0,0 +1,41 @@
+const test1 = [1, 2, 3, 4, 5]
+
+// Task: Implement a function that adds 1 to each of the items in the `array`
+// return the resulting new array
+const addPlusOne = (array) => {
+  // TODO: implement solution here
+}
+
+console.log(addPlusOne(test1)) // should show: [2,3,4,5,6]
+
+// Task: Implement a function that multiplies each item of the `array` times 2
+// return the resulting new array
+const multiplyItemsTimesTwo = (array) => {
+  // TODO: implement solution here
+}
+
+console.log(multiplyItemsTimesTwo(test1)) // should show: [2,4,6,8,10]
+
+// Task: Implement a function that appends a character to each item in the `array`
+// return the resulting new array
+const appendCharacterToItems = (array) => {
+  // TODO: implement solution here
+}
+
+console.log(multiplyItemsTimesTwo(test1)) // should show: ['x1','x2','x3','x4','x5']
+
+// Task: Implement a function that sum all the items in the `array`
+// return the resulting the total sum of the elements of the array
+const sumAllItems = (array) => {
+  // TODO: implement solution here
+}
+
+console.log(sumAllItems(test1)) // should show: 15
+
+// Task: Implement a function that removes all the "even" items of the `array`
+// return the resulting new array that only contains "odd" items
+const filterOddItems = (array) => {
+  // TODO: implement solution here
+}
+
+console.log(filterOddItems(test1)) // should show: [1,3,5]
